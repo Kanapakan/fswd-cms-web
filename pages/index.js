db2e2f292bbfb2a341d674c9d358a8338a804683
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2022-03-15 23:12:32 +0700

    fix: debug index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Home from './home'
+import React from 'react'
+// import PostContent from "../pages/posts/index"
+import styles from "../styles/Comment.module.css";
+function Home() {
+  
+    return (
+      <div className="container">
+      {/* <p> {parse(post.content.rendered)}</p> */}
+      <div className={styles.detail_con}>
+        <div className={styles.post_content}>
+            <div className="center-home-content">
+              <h1 style={{fontSize: "32px"}}>Welcome to My CMS Website!</h1>
+              <h3>Created By </h3>
+              <h3>Kanapa Kanwiwat</h3>
+              <p>This Website is part of the Full-Stack Web Developer course</p>
+            </div>
+        </div>
 
 
 
-// import Navbar from '../components/Navbar'
 
-export default function Home2() {
-  return (
-    <div className="container">
-      <Home />
-          </div>
-  )
-}
+         
+      </div>
+    </div>
+    );
+  }
+
+export default Home;
