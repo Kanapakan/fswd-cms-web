b2bc7516a578a7814ac4e7f94f13f3512eb23bdb
Kanapakan <56270434+Kanapakan@users.noreply.github.com>
2022-03-15 23:08:19 +0700

    fix: debug /Home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Home from './home'
-import PostContent from './posts'
+
 
 
 // import Navbar from '../components/Navbar'
@@ -9,7 +9,7 @@ import PostContent from './posts'
 export default function Home2() {
   return (
     <div className="container">
-      <PostContent />
+      <Home />
           </div>
   )
 }
